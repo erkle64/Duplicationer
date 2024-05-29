155d851965ae654c7ce90f6035600e82cdf15a01
erkle64 <erkle64@gmail.com>
2024-05-30 08:49:16 +1200

    Updated version number.

--- Duplicationer/Plugin.cs
+++ Duplicationer/Plugin.cs
@@ -15,7 +15,7 @@ namespace Duplicationer
             MODNAME = "duplicationer",
             AUTHOR = "erkle64",
             GUID = AUTHOR + "." + MODNAME,
-            VERSION = "0.4.7";
+            VERSION = "0.4.9";
 
         public static LogSource log;
 
